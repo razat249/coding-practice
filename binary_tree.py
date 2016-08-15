113e8a150ed529be4ad743ad0f7bda1bd8e33884
Rajat <razat249@gmail.com>
2016-08-15 15:27:01 +0530

    Add binary tree

--- binary_tree.py
+++ binary_tree.py
@@ -1,1 +1,12 @@
-Linked list binary tree as
+class Node(object):
+	def __init__(self, value):
+		self.value = value
+		self.left = None
+		self.right = None
+
+class BinaryTree(object):
+	def __init__(self, root):
+		self.root = Node(root)
+
+	def search(self, find_val):
+		
